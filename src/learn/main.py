4df37106dcd4c864b614fdaa18e984c030d31f0b
吳俊緯 <wcw850710@gmail.com>
2021-04-14 20:58:40 +0800

    feat: bool, complex

--- src/learn/main.py
+++ src/learn/main.py
@@ -1,4 +1,6 @@
 # type 基本數據類型
+#
+# Number 數字
 # int 整數(其他語言有分 short, int, long)
 # float 浮點數(單精度 float, 雙精度 double, 不過 py 沒有分)
 type(1) # <class 'int'>
@@ -9,8 +11,8 @@ type(1.1111111111111111) # <class 'float'>
 type(1 + 0.1) # <class 'float'>
 type(1 + 1.0) # <class 'float'>
 type(2 / 2) # <class 'float'> 除法返回是 float(1.0)
-
-# 補充：
+#
+# 補充
 # 10, 2, 8, 16 進制
 # 10 滿 10 進 1, e.g. 0, 1, 2, ... 9, 10, 11, ...
 # 2, e.g. 0, 1, 10, 11, 100, 101, ...
@@ -23,3 +25,20 @@ bin(10) # '0b1010' X 進制轉 2 進制
 oct(0b111) # '0o7' X 進制轉 8 進制
 int(0b10) # 2 X 進制轉 10 進制
 hex(888) # '0x378' X 進制轉 16 進制
+#
+# bool 布林(屬於數字分類的一種)
+True
+False
+type(True) # <class 'bool'>
+int(True) # 1
+int(False) # 0
+bool(1) # True (非空值(0, '', [], {}, None) 都是 True)
+bool(0) # False
+#
+# complex 複數
+36j # j 表示複數
+
+
+
+# 變量
+#
